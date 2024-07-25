35e2247057782b32bcf7bed205c724bbdb825cbe
Pragya-Dwivedi28 <117989272+Pragya-Dwivedi28@users.noreply.github.com>
2024-07-25 20:00:30 +0530

    Create ones.py
    #numpy.ones() in array

---
+++ Advanced Python/NUMPY/ones.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+#numpy.ones() in array 
+arr1 = np.ones((5,2))
+print(arr1)
+
+arr2= np.ones(6)
+print(arr2)
+#create a 1-D array of ones (with 5 elements and integer datatype)
+arr3= np.ones(5,dtype=int)
+print(arr3)
+#an array with 6 ones and integer datatype
+arr4= np.array((6,), dtype=int)
+print(arr4)
+
+#2-d array with ones
+arr5 = np.ones((3,4))
+print(arr5)
