7b41365800b4a6509856e4d5ba04a036c757b63d
Pragya-Dwivedi28 <117989272+Pragya-Dwivedi28@users.noreply.github.com>
2024-07-25 20:14:47 +0530

    Create ArrayOperations1.py
    #perform mathematical functions on array such as min(), max(), argmin(),argmax(), sqrt(), sin(), cos().

---
+++ Advanced Python/NUMPY/ArrayOperations1.py
@@ -0,0 +1,12 @@
+#perform mathematical functions on array
+
+import numpy as np
+
+arr = np.array([10,20,30,25,6,2])
+print("The minimum of array is :",np.min(arr))
+print("The maximum of array is :",np.max(arr))
+print("The index of minimum of array is :",np.argmin(arr))
+print("The index of maximum of array is :",np.argmax(arr))
+print("The square root of array is :",np.sqrt(arr))
+print("The sine value of array values are :",np.sin(arr))
+print("The cosine value of array values are :",np.cos(arr))
