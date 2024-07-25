17d8048bb3da288f4c52d87ce0a9d54312b31976
Pragya-Dwivedi28 <117989272+Pragya-Dwivedi28@users.noreply.github.com>
2024-07-25 12:49:04 +0530

    Create ndim.py
    python program for ndim() for finding out he number of dimensions in numpy

---
+++ Advanced Python/NUMPY/ndim.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+a= np.array(42)
+b= np.array([1,2,3,4,5])
+c= np.array([[1,2,3],[4,5,6]])
+d= np.array([[1,2,3],[4,5,6],[7,8,9]])
+
+#to print the array in numpy
+print("a array :",a)
+print("b array :",b)
+print("c array :",c)
+print("d array :",d)
+
+
+#to print the dimension of an array in numpy
+print("a array dimension :",a.ndim)
+print("b array dimension :",b.ndim)
+print("c array dimension :",c.ndim)
+print("d array dimension :",d.ndim)
+
