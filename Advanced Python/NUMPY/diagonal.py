5a1a1d38508c7a9ced96937a150b11187abccf33
Pragya-Dwivedi28 <117989272+Pragya-Dwivedi28@users.noreply.github.com>
2024-07-25 20:03:27 +0530

    Create diagonal.py
    import numpy as np
    
    #to find the diagonal elements of a particular array
    #to print the diagonal elements in an array

---
+++ Advanced Python/NUMPY/diagonal.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+#to find the diagonal elements of a particular array
+arr = np.array([[1,2,3],[4,5,6],[4,5,6]])
+print(np.diag(arr))
+
+#to print the diagonal elements in an array
+print(np.diag((3,5,9)))
