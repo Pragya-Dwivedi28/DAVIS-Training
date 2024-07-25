ee88405100b7a7af1d3beee60af78b8c6ccc1087
Pragya-Dwivedi28 <117989272+Pragya-Dwivedi28@users.noreply.github.com>
2024-07-25 12:50:36 +0530

    Create shape().py
    ndim() and shape() functions in numpy

---
+++ Advanced Python/NUMPY/shape().py
@@ -0,0 +1,8 @@
+#Dimension and Shape of array in numpy
+
+import numpy as np
+
+arr = np.array([[1,2,3,4,5],[6,7,8,9,4]])
+print(arr)
+print("Array dimension is :",arr.ndim)
+print("Array shape is :",arr.shape)
