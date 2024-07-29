5f3d9f888b9425ee47c1b14aaef6c509dd943eed
Pragya-Dwivedi28 <117989272+Pragya-Dwivedi28@users.noreply.github.com>
2024-07-29 11:35:58 +0530

    Create vstack.py
    #np.vstack(): Stacks arrays vertically (along rows).

---
+++ Advanced Python/NUMPY/vstack.py
@@ -0,0 +1,9 @@
+#np.vstack(): Stacks arrays vertically (along rows).
+
+import numpy as np
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([4, 5, 6])
+# Stack arr1 and arr2 vertically
+result = np.vstack((arr1, arr2))
+# Print the vertically stacked array
+print(result)
