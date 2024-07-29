c74b889143ee884923fd376316aea8e9e4aab4d6
Pragya-Dwivedi28 <117989272+Pragya-Dwivedi28@users.noreply.github.com>
2024-07-29 11:34:34 +0530

    Update sort.py
    #sort() --> used to sort the elements of an array

--- Advanced Python/NUMPY/sort.py
+++ Advanced Python/NUMPY/sort.py
@@ -11,3 +11,10 @@ sorted_arr_desc = np.sort(arr)[::-1]
 # Print the sorted array in descending order
 print("Sorted array (descending):", sorted_arr_desc)
 
+#sorting in 2D-Array
+
+matrix = np.array([[3, 1, 2],[6, 5, 5]])
+# Sort the array in ascending order (creates a new sorted array)
+sorted_matrix = np.sort(matrix, axis=1)
+# Print the sorted array
+print("Sorted matrix (ascending along rows):", sorted_matrix)
